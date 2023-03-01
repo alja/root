771cc4001808823df304579f317c1a519c03f22a
alja <amraktadel@ucsd.edu>
2023-03-01 06:14:55 -0800

    Skeleton for EveGeoBrowser.

---
+++ tutorials/eve7/eveGeoBrowser.C
@@ -0,0 +1,40 @@
+
+#include <ROOT/REveGeoTopNode.hxx>
+#include <ROOT/REveManager.hxx>
+
+
+namespace REX = ROOT::Experimental;
+
+void eveGeoBrowser()
+{
+   auto eveMng = REX::REveManager::Create();
+
+   TFile::SetCacheFileDir(".");
+
+   TGeoManager::Import("https://root.cern/files/cms.root");
+
+   gGeoManager->DefaultColors();
+   gGeoManager->SetVisLevel(4);
+   gGeoManager->GetVolume("TRAK")->InvisibleAll();
+   gGeoManager->GetVolume("HVP2")->SetTransparency(20);
+   gGeoManager->GetVolume("HVEQ")->SetTransparency(20);
+   gGeoManager->GetVolume("YE4")->SetTransparency(10);
+   gGeoManager->GetVolume("YE3")->SetTransparency(20);
+   gGeoManager->GetVolume("RB2")->SetTransparency(99);
+   gGeoManager->GetVolume("RB3")->SetTransparency(99);
+   gGeoManager->GetVolume("COCF")->SetTransparency(99);
+   gGeoManager->GetVolume("HEC1")->SetLineColor(7);
+   gGeoManager->GetVolume("EAP1")->SetLineColor(7);
+   gGeoManager->GetVolume("EAP2")->SetLineColor(7);
+   gGeoManager->GetVolume("EAP3")->SetLineColor(7);
+   gGeoManager->GetVolume("EAP4")->SetLineColor(7);
+   gGeoManager->GetVolume("HTC1")->SetLineColor(2);
+
+   auto topNode = new REX::REveGeoTopNode();
+
+
+
+   eveMng->GetEventScene()->AddElement(topNode);
+
+   eveMng->Show();
+}
