9dd32197c90c8c29822c3e55d66a508adba19da3
alja <amraktadel@ucsd.edu>
2026-03-26 10:34:00 -0700

    Rename function SetTopNodeWithPath() to InitPath()

--- tutorials/visualisation/eve7/eveGeoBrowser.C
+++ tutorials/visualisation/eve7/eveGeoBrowser.C
@@ -41,7 +41,8 @@ void eveGeoBrowser()
 
    // initialize RGeomDesc from TGeoNode
    auto data = new REveGeoTopNodeData("http://xrd-cache-1.t2.ucsd.edu/alja/mail/geo/cmsSimGeo2026.root");
-   data->SetTopNodeWithPath("/tracker:Tracker_1");
+   // data->InitPath("/tracker:Tracker_1");
+   data->InitPath("/");
    data->RefDescription().SetVisLevel(2);
 
    // make geoTable
